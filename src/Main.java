cf3771668e711a718ce11e62b52e4a33f853538d
Daisuke Nakama <e215716@ie.u-ryukyu.ac.jp>
2021-11-02 09:41:37 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import jp.ac.uryukyuu.ie.*;
+
+public class Main {
+    public static void main(String arg[]){
+        System.out.println("main method runs");
+
+        Chatbot bot1 = new Chatbot();
+    }
+}    
---
+++ src/jp/ac/uryukyuu/ie/Chatbot.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyuu.ie;
+
+public class Chatbot {
+    
+    public Chatbot(){
+        System.out.println("チャットボットの原型を作成します");
+    }
+}
